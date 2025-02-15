5a6c16f61d8fd8ac768491941b4d1891aaf0c6fd
unknown <bajohn09@gmail.com>
2025-02-15 10:55:31 -0500

    adding Database Code

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Float, TIMESTAMP, func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# MODELOS DE DATOS!!! no analiticos
+#Pydantic es mas para input y output de un endpoint no para una base de datos
+
+class Prediction(Base):
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    file_name = Column(String, nullable=False)
+    prediction = Column(Float, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=True) 
